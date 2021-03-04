cac2556f06fbbe60f392debd197c9ab9cfe936ce
Tyler Pentecost <tyler8806@gmail.com>
2021-03-04 11:21:35 -0600

    Update RollTrackerClassic.lua

--- RollTrackerClassic.lua
+++ RollTrackerClassic.lua
@@ -5,7 +5,7 @@ local L = setmetatable({}, {
     end
 })
 
-RollTrackerClassic_Addon = RTC -- luacheck: unused
+RollTrackerClassic_Addon = RTC -- luacheck: ignore unused
 RTC.Version = GetAddOnMetadata(TOCNAME, "Version")
 RTC.Title = GetAddOnMetadata(TOCNAME, "Title")
 
