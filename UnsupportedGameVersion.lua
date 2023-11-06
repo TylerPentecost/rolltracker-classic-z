6d04fdeecd3f70bdd2b47a22946cbe6849b536a6
Tyler Pentecost <tyler8806@gmail.com>
2023-11-06 01:21:35 +0000

    Moved to use multiple TOC files

---
+++ UnsupportedGameVersion.lua
@@ -0,0 +1,8 @@
+StaticPopupDialogs["RTCZ_VERSION_ERROR"] = {
+    text = "Unsupported game version for" .. "\n" .. "RolltrackerClassicZ",
+    button2 = "OK",
+    hasEditBox = false,
+    whileDead = true
+}
+
+StaticPopup_Show("RTCZ_VERSION_ERROR")
